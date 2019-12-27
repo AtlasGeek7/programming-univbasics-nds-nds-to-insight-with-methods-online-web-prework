ba0cc7f6533440fadfee6e2dea8286d64fcdfe73
Khalid Tissire <ghettoguy7@gmail.com>
2019-12-27 21:57:03 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,25 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
-  nil
+  i = 0
+  len = nds.length
+  while i < len do
+    sum = gross_for_director(nds[i])
+    result[nds[i][:name]] = sum
+    i += 1
+  end
+  return result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+    sum = 0
+    j = 0
+    len = director_data[:movies].length 
+    while j < len do
+      sum += director_data[:movies][j][:worldwide_gross]
+      j += 1
+    end
+  return sum
 end
